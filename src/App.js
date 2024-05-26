15652f82361aef809f5503c3b6ebe7373968fd08
ONEABBOTT\pitlakx <kamal.pitla@abbott.com>
2024-05-26 17:16:02 -0500

    products fetch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import 'react-data-grid/lib/styles.css';
+import { useState, useEffect } from 'react';
+import 'react-data-grid/lib/styles.css';
+import DataGrid from 'react-data-grid';
 
+let columns = [];
+
+let rows = [];
+
+function extractColRows(data){
+   columns = Object.keys(data[0]);
+   rows = data;
+   console.log(columns);
+   console.log(columns)
+}
 
 function App() {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setProductsData(data);
+        extractColRows(data)
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Hello world</Button>
-      </header>
+      {/* <DataGrid columns={columns} rows={rows} />; */}
+      {/* {photos.map((photo) => (
+        <img key={photo.id} src={photo.url} alt={photo.title} width={100} />
+      ))} */}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+[{
+  "key":columns[0],
+  "name":columns[0]
+
+},{
+  "key":columns[1],
+  "name":columns[1]
+
+}]
